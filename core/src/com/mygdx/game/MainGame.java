9be120c68b178e4a1c95a87f5de6c0c418dc4cf2
zliving <zachary.n.livingston@gmail.com>
2016-02-22 18:13:51 -0600

    New ScenceLoader

--- core/src/com/mygdx/game/MainGame.java
+++ core/src/com/mygdx/game/MainGame.java
@@ -5,23 +5,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.uwsoft.editor.renderer.SceneLoader;
+
+
 
 public class MainGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	public SceneLoader sceneLoader;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Viewport viewport = new FitViewport(800, 480);
+		SceneLoader sceneLoader = new SceneLoader();
+		sceneLoader.loadScene("MainScene");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		sceneLoader.getEngine().update(Gdx.graphics.getDeltaTime());
 	}
 }
