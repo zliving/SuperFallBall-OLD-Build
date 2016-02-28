3cca5fda963aa7ef50ce5b30b174b89f0e063544
Chris.V <cvasqu09@gmail.com>
2016-02-27 20:37:13 -0600

    Finished the prototype collision detection for the ball and door.

--- core/src/GameObjects/Ball.java
+++ core/src/GameObjects/Ball.java
@@ -13,53 +13,44 @@ import com.uwsoft.editor.renderer.components.TransformComponent;
 import com.uwsoft.editor.renderer.scripts.IScript;
 import com.uwsoft.editor.renderer.utils.ComponentRetriever;
 
+import org.w3c.dom.css.Rect;
+
+import sun.applet.Main;
+
 //New Branch merge test
 public class Ball implements IScript {
 
-    //FIXME: Add collision detection method.
     private Entity ballEntity;
     private TransformComponent transformComponent;
     private float scaleUnits = 3.0f;
+    private Rectangle collisionRect;
 
-    private float x;
-    private float y;
     private float velocity = 1.0f;
     private float gravity = 2.0f;
-    private float radius = 36.0f; //World units is given in overlap
+    private float radius = 12; //World units
+    private float height, width;
 
-    private Circle collisionCircle;
-    private ShapeRenderer debugRenderer;
+    public boolean colliding = false;
 
     @Override
     public void init(Entity entity) {
+        width = height = 2 * radius;
         ballEntity = entity;
         transformComponent = ComponentRetriever.get(entity, TransformComponent.class);
-        //Transform component is given in world units
-        x = transformComponent.x * scaleUnits;
-        y = transformComponent.y * scaleUnits;
-
-        collisionCircle = new Circle(x + radius, y + radius, radius);
-        debugRenderer = new ShapeRenderer();
-        System.out.println("transformComponent x: " + transformComponent.x);
-        System.out.println("transformComponent y: " + transformComponent.y);
-        //Testing
-        System.out.println("collision circle x: " + collisionCircle.x);
-        System.out.println("collision circle y: " + collisionCircle.y);
+        collisionRect = new Rectangle(transformComponent.x, transformComponent.y, width, height);
     }
 
     @Override
     public void act(float delta) {
-        debugRenderer.begin(ShapeRenderer.ShapeType.Line);
-        debugRenderer.circle(collisionCircle.x, collisionCircle.y, radius);
-        debugRenderer.end();
 
         //If ball has reached the floor
-        if(y <= 0){
+        if(collisionRect.getY() <= 0){
             transformComponent.y = 0;
         } else {
-//            transformComponent.y -= gravity;   //Gravity always acting on the ball;
-            return;
+            transformComponent.y -= gravity;   //Gravity always acting on the ball;
+
         }
+
 
         //Movement
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
@@ -74,8 +65,16 @@ public class Ball implements IScript {
             transformComponent.y += velocity;
         }
 
-        update();
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            transformComponent.y -= velocity;
+        }
 
+        updateBounds();
+//        Testing
+//        System.out.println("Transform ball x: " + transformComponent.x);
+//        System.out.println("Transform ball y: " + transformComponent.y);
+//        System.out.println("Collision ball x: " + collisionRect.getX());
+//        System.out.println("Collision ball y: " + collisionRect.getY());
     }
 
     @Override
@@ -83,10 +82,41 @@ public class Ball implements IScript {
 
     }
 
-    //Update the collision circle
-    public void update(){
-        collisionCircle.setX((transformComponent.x * scaleUnits) + radius);
-        collisionCircle.setY((transformComponent.y * scaleUnits) + radius);
+    public void updateBounds(){
+        collisionRect.setX(transformComponent.x);
+        collisionRect.setY(transformComponent.y);
     }
 
+    //Getters and setters
+    public float getX() {
+        return transformComponent.x;
+    }
+
+    public void setX(float x) {
+        transformComponent.x = x;
+    }
+
+    public float getY() {
+        return transformComponent.y;
+    }
+
+    public void setY(float y) {
+        transformComponent.y = y;
+    }
+
+    public float getWidth(){
+        return 2 * radius;
+    }
+
+    public float getHeight(){
+        return 2 * radius;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public Rectangle getCollisionRect(){
+        return collisionRect;
+    }
 }
--- core/src/GameObjects/Door.java
+++ core/src/GameObjects/Door.java
@@ -3,7 +3,9 @@ package GameObjects;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.game.MainGame;
 import com.uwsoft.editor.renderer.components.TransformComponent;
 import com.uwsoft.editor.renderer.scripts.IScript;
 import com.uwsoft.editor.renderer.utils.ComponentRetriever;
@@ -11,40 +13,71 @@ import com.uwsoft.editor.renderer.utils.ComponentRetriever;
 public class Door implements IScript{
     //Size for Door object
     //These sizes are temporary
-    private float SIZEX = 27f;
-    private float SIZEY = 46f;
-    private Rectangle collisionRect;
-    private float scaleCollisionRect = .15f;
-    private float scaleWorldUnits = 3.0f;
+    private float width = 27.0f;
+    private float height = 46.0f;
 
+    //For collision rectangle
+    private float scale = 0;
+    private float scaledWidth;
+    private float scaledHeight;
+
+    private Rectangle collisionRect;
     //May need entity for later but it is currently unused right now.
     private Entity doorEntity;
 
     //The transform component contains the position (x, y) of the door
     private TransformComponent transformComponent;
 
-    //FIXME: Add collision detection method.
+    //Need to import the ball so that we can check for collision.
+    private Ball ball;
 
     @Override
     public void init(Entity entity) {
         doorEntity = entity;
         transformComponent = ComponentRetriever.get(entity, TransformComponent.class);
+        scaledWidth = width - (2 * scale * width);
+        scaledHeight = height - (2 * scale * height);
+        collisionRect = new Rectangle(transformComponent.x + width * scale,
+                transformComponent.y + height * scale, scaledWidth, scaledHeight);
 
-        collisionRect = new Rectangle(transformComponent.x + SIZEX * scaleCollisionRect,
-                transformComponent.y + SIZEY * scaleCollisionRect,
-                SIZEX * (1.0f - 2.0f * scaleCollisionRect),     //scale is multiplied by 2 since we
-                SIZEY * (1.0f - 2.0f * scaleCollisionRect));    //trim off from both sides
-
+        System.out.println("x: " + transformComponent.x);
+        System.out.println("y: " + transformComponent.y);
+        System.out.println("x coll: " + collisionRect.getX());
+        System.out.println("y coll: " + collisionRect.getY());
+        System.out.println("coll width: " + collisionRect.getWidth());
+        System.out.println("coll height: " + collisionRect.getHeight());
+        ball = MainGame.ball;
         //Testing for the collision rectangle
-//        System.out.println("collision x: " + collisionRect.getX());
-//        System.out.println("collision y: " + collisionRect.getY());
-//        System.out.println("Collision width: " + collisionRect.getWidth());
-//        System.out.println("Collision height: " + collisionRect.getHeight());
+        System.out.println("collision x: " + collisionRect.getX());
+        System.out.println("collision y: " + collisionRect.getY());
+        System.out.println("Collision width: " + collisionRect.getWidth());
+        System.out.println("Collision height: " + collisionRect.getHeight());
     }
 
     @Override
     public void act(float delta) {
+        if(isColliding(ball)){
+            System.out.println("Colliding");
+            //Collides from the left side
+            if(ball.getCollisionRect().getX() <= collisionRect.getX()) {
+                ball.setX(transformComponent.x - ball.getWidth());
+            }
 
+            //Collides from the right side of the door
+            if(ball.getCollisionRect().getX() > collisionRect.getX()){
+                ball.setX(transformComponent.x + width);
+            }
+
+            //Collides from the bottom
+            if(ball.getCollisionRect().getY() <= collisionRect.getY()){
+                ball.setY(transformComponent.y - ball.getHeight());
+            }
+
+            //Collides from the top
+            if(ball.getCollisionRect().getY() > collisionRect.getY()){
+                ball.setY(transformComponent.y + height);
+            }
+        }
     }
 
     @Override
@@ -52,6 +85,11 @@ public class Door implements IScript{
 
     }
 
+    public boolean isColliding(Ball b){
+        return b.getCollisionRect().overlaps(collisionRect);
+    }
+
+    //Getters and Setters
     public float getX(){
         return transformComponent.x;
     }
@@ -59,4 +97,13 @@ public class Door implements IScript{
     public float getY(){
         return transformComponent.y;
     }
+
+    public float getWidth(){
+        return width;
+    }
+
+    public float getHeight(){
+        return height;
+    }
+
 }
