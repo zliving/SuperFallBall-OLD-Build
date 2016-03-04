5ea000119b9b2e2acd2dbe4fcc7df5d62d4a2caf
zliving <zachary.n.livingston@gmail.com>
2016-03-04 15:32:49 -0600

    New Background!!!!!!!! Also layered system for ball and doors

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.mygdx.game.MainGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 640;
-		config.height = 960;
+		config.width = 620;
+		config.height = 940;
 		new LwjglApplication(new MainGame(), config);
 	}
 }
